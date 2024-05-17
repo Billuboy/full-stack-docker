2923fc5827e248a555010768d850357e1581afeb
Billuboy <manujk171801@gmail.com>
2024-05-17 18:50:14 +0530

    Created K8s resources for the application

--- backend/drizzle/migrations/migrate.js
+++ backend/drizzle/migrations/migrate.js
@@ -6,8 +6,8 @@ const { migrate } = require('drizzle-orm/postgres-js/migrator');
 const postgres = require('postgres');
 
 dotenv.config({ path: resolve('../../', '.env.development') });
-
-const migrationClient = postgres(process.env.DB_CONN_STRING, { max: 1 });
+const MIG_CONN_STRING = process.env.DB_CONN_STRING;
+const migrationClient = postgres(MIG_CONN_STRING, { max: 1 });
 
 migrate(drizzle(migrationClient), {
   migrationsFolder: './',
